ea20a8c0b24abb9fe0efdb3c34edd71455068687
Joanne Cai <joanne.luyang.cai@gmail.com>
2026-09-02 08:04:38 -0400

    fix artist photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from 'react'
 import paintingData from '../Painting_info - Data.csv?raw'
 import heroPainting from '../paintings/Cai002.jpeg'
-import aboutPhoto from './assets/about-photo-placeholder.svg'
+import aboutPhoto from './assets/JoannePhoto.png'
 import './App.css'
 
 const baseUrl = import.meta.env.BASE_URL
